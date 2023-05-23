acb337882c6678a75ceb129930a51112e18fa423
whonion <root@whonion.app>
2023-05-23 16:46:54 +0800

    commit testing code

---
+++ config.py
@@ -0,0 +1,30 @@
+EXP_ETH = 'https://etherscan.io/tx/'
+EXP_ARB = 'https://arbiscan.io/tx/'
+EXP_OPT = 'https://optimistic.etherscan.io/tx/'
+EXP_BSC = 'https://bscscan.com/tx/'
+EXP_MATIC = 'https://polygonscan.com/tx/'
+
+
+MIN_ETH_TO_ARB = 0.0021
+MIN_ETH_TO_OPT = 0.0045
+MIN_ETH_TO_BSC = 0.00045
+MIN_ETH_TO_MATIC = 0.00014
+#MIN_ARB_TO_ETH = ''
+MIN_ARB_TO_OPT = 0.0045
+MIN_ARB_TO_BSC = 0.00045
+MIN_ARB_TO_MATIC = 0.00021
+
+#MIN_OPT_TO_ETH = ''
+MIN_OPT_TO_ARB = 0.003
+MIN_OPT_TO_BSC = 0.00045
+MIN_OPT_TO_MATIC = 0.00021
+
+#MIN_BSC_TO_ETH = ''
+MIN_BSC_TO_ARB = 0.012
+MIN_BSC_TO_OPT = 0.027
+MIN_BSC_TO_MATIC = 0.0013
+
+#MIN_MATIC_TO_ETH = ''
+MIN_MATIC_TO_OPT = 9.32
+MIN_MATIC_TO_ARB = 4.19
+MIN_MATIC_TO_BSC = 0.93
