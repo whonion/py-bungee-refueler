45e98b33203ec32cf8e5a412abb8fe10474e7c88
RegEdit | whonion.app <47848969+whonion@users.noreply.github.com>
2023-05-31 23:07:25 +0800

    update refuel.py | reqiured commit

--- refuel.py
+++ refuel.py
@@ -18,37 +18,75 @@ def getGasLimit(chain:str)->int:
           gas_limit = data['result'][4]['gasLimit']
           return gas_limit
      elif chain == 'ERA':
-          gas_limit = data['result'][5]['gasLimit']
+          gas_limit = data['result'][6]['gasLimit']
           return gas_limit     
      elif chain == 'ZKEVM':
-          gas_limit = data['result'][6]['gasLimit']
-          return gas_limit    
-     elif chain == 'ARB':
           gas_limit = data['result'][7]['gasLimit']
           return gas_limit    
-     elif chain == 'AVAX':
+     elif chain == 'ARB':
           gas_limit = data['result'][8]['gasLimit']
-          return gas_limit
-     elif chain == 'AUR':
+          return gas_limit    
+     elif chain == 'AVAX':
           gas_limit = data['result'][9]['gasLimit']
           return gas_limit
-    #  elif chain == 'FTM':
-    #       gas_limit = data['result'][nil]['gasLimit']
-    #       return gas_limit
-      
-def getMinSendAmount(parent_chainId,destination_chainId) -> float:
-    # Get the value of 'minAmount'
-    min_amount = float(data['result'][parent_chainId]['limits'][destination_chainId]['minAmount'])
-    min_amount = min_amount / 10**18
-    #print(f"Minimum Amount: {min_amount}")
-    return min_amount
+     elif chain == 'AUR':
+          gas_limit = data['result'][10]['gasLimit']
+          return gas_limit
+     elif chain == 'FTM':
+          gas_limit = data['result'][5]['gasLimit']
+          return gas_limit
+def getMinSendAmount(parent_chain, destination_chain_id) -> float:
+    try:
+        # Check if the required keys exist in the JSON data
+        if 'result' in data and isinstance(data['result'], list):
+            for chain in data['result']:
+                if 'name' in chain and chain['name'] == parent_chain:
+                    parent_result = chain
+                    break
+            else:
+                return None  # Parent chain not found
+            
+            if 'limits' in parent_result and isinstance(parent_result['limits'], list):
+                for limit in parent_result['limits']:
+                    if 'chainId' in limit and limit['chainId'] == destination_chain_id:
+                        destination_limits = limit
+                        break
+                else:
+                    return None  # Destination chain not found
+                    
+                if 'minAmount' in destination_limits:
+                    min_amount = float(destination_limits['minAmount'])
+                    return min_amount / 10**18
+    except (KeyError, IndexError, ValueError):
+        pass  # Handle the error gracefully, e.g., return a default value or raise an exception
+    return None  # Return None if the values could not be retrieved
 
-def getMaxSendAmount(parent_chainId,destination_chainId) -> float:
-    # Get the value of 'maxAmount'
-    max_amount = float(data['result'][parent_chainId]['limits'][destination_chainId]['maxAmount'])
-    max_amount = max_amount / 10**18
-    #print(f"Maximum Amount: {max_amount}")
-    return max_amount
+
+def getMaxSendAmount(parent_chain, destination_chain_id) -> float:
+    try:
+        # Check if the required keys exist in the JSON data
+        if 'result' in data and isinstance(data['result'], list):
+            for chain in data['result']:
+                if 'name' in chain and chain['name'] == parent_chain:
+                    parent_result = chain
+                    break
+            else:
+                return None  # Parent chain not found
+            
+            if 'limits' in parent_result and isinstance(parent_result['limits'], list):
+                for limit in parent_result['limits']:
+                    if 'chainId' in limit and limit['chainId'] == destination_chain_id:
+                        destination_limits = limit
+                        break
+                else:
+                    return None  # Destination chain not found
+                    
+                if 'maxAmount' in destination_limits:
+                    max_amount = float(destination_limits['maxAmount'])
+                    return max_amount / 10**18
+    except (KeyError, IndexError, ValueError):
+        pass  # Handle the error gracefully, e.g., return a default value or raise an exception
+    return None  # Return None if the values could not be retrieved
 
 url = "https://refuel.socket.tech/chains"
 output_file = "chains.json"  # File name to save the JSON data
